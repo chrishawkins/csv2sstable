ba608e4dc9fb0451bdb1c7e63e52946d47955c5e
Dmytro Popovych <drudim.ua@gmail.com>
2015-02-21 19:01:25 +0200

    Add insert statement generation

--- src/main/java/com/tubularlabs/CsvToSSTable.java
+++ src/main/java/com/tubularlabs/CsvToSSTable.java
@@ -1,12 +1,17 @@
 package com.tubularlabs;
 
 import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.config.Config;
 import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.cql3.statements.CreateTableStatement;
 import org.apache.cassandra.exceptions.RequestValidationException;
 import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.LinkedHashMap;
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class CsvToSSTable {
@@ -68,17 +73,23 @@ public class CsvToSSTable {
     }
 
     private static void migrate(String cqlStatement, String mappingDefinition, String csvPath, String outputPath) {
-        LinkedHashMap<String, Integer> mapping = readMapping(mappingDefinition);
+        HashMap<String, Integer> mapping = readMapping(mappingDefinition);
         CFMetaData cfMetaData = getCFMetadata(cqlStatement);
+        List<String> mappedFields = new LinkedList<String>(mapping.keySet());
+        String insertStatement = getInsertStatement(cfMetaData, mappedFields);
 
-        System.out.println(mapping);
-        System.out.println(cfMetaData);
-        System.out.println(csvPath);
-        System.out.println(outputPath);
+        // magic from original snippet (https://github.com/yukim/cassandra-bulkload-example)
+        Config.setClientMode(true);
+
+        // Create output directory that has keyspace and table name in the path
+        File outputDir = new File(outputPath + File.separator + cfMetaData.ksName + File.separator + cfMetaData.cfName);
+        if (!outputDir.exists() && !outputDir.mkdirs()) {
+            throw new RuntimeException("Cannot create output directory: " + outputDir);
+        }
     }
 
-    private static LinkedHashMap<String,Integer> readMapping(String mapping) {
-        LinkedHashMap<String, Integer> parsedMapping = new LinkedHashMap<String, Integer>();
+    private static HashMap<String,Integer> readMapping(String mapping) {
+        HashMap<String, Integer> parsedMapping = new HashMap<String, Integer>();
 
         for (String fieldDefinition: mapping.split(",")) {
             String[] nameAndValue = fieldDefinition.split(":");
@@ -105,5 +116,15 @@ public class CsvToSSTable {
         }
 
         return cfMetaData;
+    }
+
+    private static String getInsertStatement(CFMetaData cfMetaData, List<String> fields) {
+        return String.format(
+                "INSERT INTO %s.%s (%s) VALUES (%s)",
+                cfMetaData.ksName,
+                cfMetaData.cfName,
+                StringUtils.join(fields, ", "),
+                StringUtils.repeat("?", ", ", fields.size())
+        );
     }
 }
